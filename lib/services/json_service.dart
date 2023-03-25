c7a85688e3add59e1528930688767a8099eaebfd
comeP <come.picard@gmail.com>
2023-03-25 19:47:47 +0100

    :sparkles: FEAT service : ajout de la fonction getChampionData

--- lib/services/json_service.dart
+++ lib/services/json_service.dart
@@ -2,6 +2,11 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
+import '../template/champion.dart';
+import '../template/champion_skin.dart';
+import '../template/champion_stats.dart';
+import '../template/spell.dart';
+
 class JsonService {
   static String listeChampionUrl =
       "https://ddragon.leagueoflegends.com/cdn/13.6.1/data/en_US/champion.json";
@@ -25,7 +30,83 @@ class JsonService {
     final response = await http.get(Uri.parse("${championDataUrl + name}.json"),
         headers: {'Access-Cross-Allow-Origin': '*'});
     final jsonData = jsonDecode(response.body);
-    print(jsonData['data']);
-    //#TODO: récupérer les données et les mettre comme il faut dans l'objet champion et tout et tout
+
+    Spell qSpell = Spell(
+        idSpell: jsonData['data']['$name']['spells'][0]['id'],
+        nom: jsonData['data']['$name']['spells'][0]['name'],
+        spellKey: "Q",
+        description: jsonData['data']['$name']['spells'][0]['description'],
+        cooldown: jsonData['data']['$name']['spells'][0]['cooldown'],
+        manaCost: jsonData['data']['$name']['spells'][0]['cost']);
+
+    Spell wSpell = Spell(
+        idSpell: jsonData['data']['$name']['spells'][1]['id'],
+        nom: jsonData['data']['$name']['spells'][1]['name'],
+        spellKey: "W",
+        description: jsonData['data']['$name']['spells'][1]['description'],
+        cooldown: jsonData['data']['$name']['spells'][1]['cooldown'],
+        manaCost: jsonData['data']['$name']['spells'][1]['cost']);
+
+    Spell eSpell = Spell(
+        idSpell: jsonData['data']['$name']['spells'][2]['id'],
+        nom: jsonData['data']['$name']['spells'][2]['name'],
+        spellKey: "E",
+        description: jsonData['data']['$name']['spells'][2]['description'],
+        cooldown: jsonData['data']['$name']['spells'][2]['cooldown'],
+        manaCost: jsonData['data']['$name']['spells'][2]['cost']);
+
+    Spell rSpell = Spell(
+        idSpell: jsonData['data']['$name']['spells'][3]['id'],
+        nom: jsonData['data']['$name']['spells'][3]['name'],
+        spellKey: "R",
+        description: jsonData['data']['$name']['spells'][3]['description'],
+        cooldown: jsonData['data']['$name']['spells'][3]['cooldown'],
+        manaCost: jsonData['data']['$name']['spells'][3]['cost']);
+
+    Passive passive = Passive(
+      idPassive: jsonData['data']['$name']['passive']['image']['full'],
+      nom: jsonData['data']['$name']['passive']['name'],
+      description: jsonData['data']['$name']['passive']['description'],
+    );
+
+    ChampionStats stats = ChampionStats(
+        hp: jsonData['data']['$name']['stats']['hp'],
+        hpLvl: jsonData['data']['$name']['stats']['hpperlevel'],
+        mana: jsonData['data']['$name']['stats']['mp'],
+        manaLvl: jsonData['data']['$name']['stats']['mpperlevel'],
+        movementSpeed: jsonData['data']['$name']['stats']['movespeed'],
+        armor: jsonData['data']['$name']['stats']['armor'],
+        armorLvl: jsonData['data']['$name']['stats']['armorperlevel'],
+        magicResistance: jsonData['data']['$name']['stats']['spellblock'],
+        magicResistanceLvl: jsonData['data']['$name']['stats']
+        ['spellblockperlevel'],
+        ad: jsonData['data']['$name']['stats']['attackdamage'],
+        adLvl: jsonData['data']['$name']['stats']['attackdamageperlevel'],
+        attackSpeed: jsonData['data']['$name']['stats']['attackspeed'],
+        attackSpeedLvl: jsonData['data']['$name']['stats']
+        ['attackspeedperlevel'],
+        attackRange: jsonData['data']['$name']['stats']['attackrange']);
+
+    List<ChampionSkin>? skins = [];
+
+    jsonData['data']['$name']['skins'].forEach((skin) {
+      ChampionSkin champSkin =
+      ChampionSkin(id: skin['num'], name: skin['name'], championName: name);
+      skins.add(champSkin);
+    });
+
+    Champion champ = Champion(
+      name: jsonData['data']['$name']["id"],
+      title: jsonData['data']['$name']["title"],
+      stats: stats,
+      skins: skins,
+      qSpell: qSpell,
+      wSpell: wSpell,
+      eSpell: eSpell,
+      rSpell: rSpell,
+      passive: passive,
+    );
+
+    return champ;
   }
 }
